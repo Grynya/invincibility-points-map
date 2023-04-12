f90c055d637a5bd0dda862c68343ce9d813aaff3
Grynya <grinya1900@gmail.com>
2023-04-12 18:16:40 +0300

    added point fetching

---
+++ src/main/java/com/invincibilitypoints/invincibilitypointsmap/dto/MapPointDto.java
@@ -0,0 +1,19 @@
+package com.invincibilitypoints.invincibilitypointsmap.dto;
+
+import com.invincibilitypoints.invincibilitypointsmap.model.MapPoint;
+import com.invincibilitypoints.invincibilitypointsmap.model.PointPhoto;
+import com.invincibilitypoints.invincibilitypointsmap.model.Resource;
+
+import java.util.Set;
+
+public record MapPointDto(Long id, String name, String description, String hoursOfWork,
+                          String phone, CoordinateDto coordinates, Set<PointPhoto> photos, Long userId,
+                          Set<Resource> resources) {
+
+    public static MapPointDto fromPoint(MapPoint point) {
+        return new MapPointDto(point.getId(), point.getName(), point.getDescription(),
+                point.getHoursOfWork(), point.getPhone(), new CoordinateDto(point.getCoordinates().getX(),
+                point.getCoordinates().getY()),
+                point.getPhotos(), point.getUserOwner().getId(), point.getResources());
+    }
+}
